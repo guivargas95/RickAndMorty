584d839ea32f5c863561aa79a9da1383dcb7b4f7
Guilherme Vargas <guisouzavargas@outlook.com>
2023-03-29 08:40:07 -0300

    first commit

---
+++ src/main.tsx
@@ -0,0 +1,19 @@
+import React from 'react'
+import ReactDOM from 'react-dom/client'
+import {
+  BrowserRouter,
+  Routes,
+  Route,
+} from "react-router-dom";
+import Home from './pages/home';
+import './config.css'
+
+ReactDOM.createRoot(document.getElementById('root') as HTMLElement).render(
+  <React.StrictMode>
+    <BrowserRouter>
+      <Routes>
+        <Route path="/" element={<Home />} />
+      </Routes>
+    </BrowserRouter>
+  </React.StrictMode>
+)
---
+++ src/pages/home/index.tsx
@@ -0,0 +1,8 @@
+export default function Home(){
+
+    return(
+        <div>
+            <p className="bg-black">Home Page</p>
+        </div>
+    )
+}
