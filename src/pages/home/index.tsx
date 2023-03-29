6163d72a2f67f76c0c08add3ed9451651f94ad29
Guilherme Vargas <guisouzavargas@outlook.com>
2023-03-29 09:38:04 -0300

    added background images

--- src/pages/home/index.tsx
+++ src/pages/home/index.tsx
@@ -1,8 +1,8 @@
 export default function Home(){
 
     return(
-        <div>
-            <p className="bg-black">Home Page</p>
+        <div className="bg-background-mobile md:bg-background bg-cover bg-repeat-0 h-140 md:h-150">
+    
         </div>
     )
 }
--- tailwind.config.cjs
+++ tailwind.config.cjs
@@ -1,9 +1,23 @@
 /** @type {import('tailwindcss').Config} */
 module.exports = {
-  content: ["./src/pages/**/*.tsx",
-  "./src/components/**/**/*.tsx",],
-theme: {
-    extend: {},
-  },
-  plugins: [],
+    content: ["./src/pages/**/*.tsx",
+        "./src/components/**/**/*.tsx",],
+    theme: {
+        extend: {
+            backgroundImage: {
+                'background-desktop': "url('/img/background-desktop.png')",
+                'background-mobile': "url('/img/background-mobile.png')",
+                'background-tablet': "url('/img/background-tablet.png')",
+                'background': "url('/img/background.png')",
+            },
+            height: {
+                '150': '58rem',
+                '140': '45rem',
+                '128': '32rem',
+                '112': '28rem',
+
+            },
+        },
+    },
+    plugins: [],
 }
