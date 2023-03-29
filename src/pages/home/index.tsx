345e69a3ffa020876f8f64760c65d889e137648a
Guilherme Vargas <guisouzavargas@outlook.com>
2023-03-29 10:29:41 -0300

    changed home page design

--- src/pages/home/index.tsx
+++ src/pages/home/index.tsx
@@ -1,8 +1,16 @@
-export default function Home(){
+export default function Home() {
 
-    return(
-        <div className="bg-background-mobile md:bg-background bg-cover bg-repeat-0 h-140 md:h-150">
-    
+    return (
+        <div className="flex bg-background-mobile md:bg-background bg-cover bg-current bg-no-repeat h-140 md:h-150">
+            <div className="flex text-center mr-auto ml-auto mt-auto mb-auto border border-black h-36">
+                <div className="mt-auto mb-auto">
+                    <h1>Welcome to asd asd asd asdasd</h1>
+                    <button className="text-white bg-black rounded-md h-8 w-12">Enter</button>
+                </div>
+
+
+            </div>
+
         </div>
     )
 }
