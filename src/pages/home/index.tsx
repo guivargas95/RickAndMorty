959373b94d6a4a5c5d85a4be1ff52fa6027c69d2
Guilherme Vargas <guisouzavargas@outlook.com>
2023-03-29 11:44:29 -0300

    responsive changes

--- src/pages/home/index.tsx
+++ src/pages/home/index.tsx
@@ -3,11 +3,11 @@ export default function Home() {
     return (
         <div className="flex bg-background-mobile md:bg-background bg-cover bg-current bg-no-repeat h-screen">
             <div className="flex flex-col text-center mr-auto ml-auto mt-auto mb-auto">
-                <img className="bounceInDown" src="/img/Logo.png" alt="" />
+                <img className="bounceInDown lg:w-140" src="/img/Logo.png" alt="" />
                 <div className="mt-5 mb-5 flex flex-col ml-auto mr-auto backInLeft">
-                    <button className="text-white bg-purple-900 rounded-md h-8 w-24 mt-2 mb-2">Characters</button>
-                    <button className="text-white bg-purple-900 rounded-md h-8 w-24 mt-2 mb-2">Locations</button>
-                    <button className="text-white bg-purple-900 rounded-md h-8 w-24 mt-2 mb-2">Episodes</button>
+                    <button className="text-white bg-purple-900 rounded-md h-8 w-24 mt-2 mb-2 lg:text-2xl lg:w-32">Characters</button>
+                    <button className="text-white bg-purple-900 rounded-md h-8 w-24 mt-2 mb-2 lg:text-2xl lg:w-32">Locations</button>
+                    <button className="text-white bg-purple-900 rounded-md h-8 w-24 mt-2 mb-2 lg:text-2xl lg:w-32">Episodes</button>
                 </div>
 
 
