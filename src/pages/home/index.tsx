5f88b9429ad7857b36dd4cdf0551cc1a971c8b0a
Guilherme Vargas <guisouzavargas@outlook.com>
2023-03-29 11:35:11 -0300

    added animation lib

--- src/pages/home/index.tsx
+++ src/pages/home/index.tsx
@@ -3,9 +3,8 @@ export default function Home() {
     return (
         <div className="flex bg-background-mobile md:bg-background bg-cover bg-current bg-no-repeat h-screen">
             <div className="flex flex-col text-center mr-auto ml-auto mt-auto mb-auto">
-                <img src="/img/Logo.png" alt="" />
-                <h1 className="text-3xl">Welcome to asd asd asd asdasd</h1>
-                <div className="mt-5 mb-5 flex flex-col ml-auto mr-auto">
+                <img className="bounceInDown" src="/img/Logo.png" alt="" />
+                <div className="mt-5 mb-5 flex flex-col ml-auto mr-auto backInLeft">
                     <button className="text-white bg-purple-900 rounded-md h-8 w-24 mt-2 mb-2">Characters</button>
                     <button className="text-white bg-purple-900 rounded-md h-8 w-24 mt-2 mb-2">Locations</button>
                     <button className="text-white bg-purple-900 rounded-md h-8 w-24 mt-2 mb-2">Episodes</button>
