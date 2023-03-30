08ed9471c24fe61461fa77b82c05d69d162127c6
Guilherme Vargas <guisouzavargas@outlook.com>
2023-03-30 12:36:35 -0300

    changed characters link

--- src/pages/home/index.tsx
+++ src/pages/home/index.tsx
@@ -7,7 +7,7 @@ export default function Home() {
             <div className="flex flex-col text-center mr-auto ml-auto mt-auto mb-auto">
                 <img className="animate-bounce lg:w-140" src="/img/Logo.png" alt="" />
                 <div className="mt-5 mb-5 flex flex-col ml-auto mr-auto zoomInUp">
-                    <Link to="/characters"><button className="text-white bg-purple-900 rounded-md h-8 w-24 mt-2 mb-2 hover:font-bold lg:text-2xl lg:w-32">Characters</button></Link>
+                    <Link to="/characters/1"><button className="text-white bg-purple-900 rounded-md h-8 w-24 mt-2 mb-2 hover:font-bold lg:text-2xl lg:w-32">Characters</button></Link>
                     <button className="text-white bg-purple-900 rounded-md h-8 w-24 mt-2 mb-2 hover:font-bold lg:text-2xl lg:w-32">Locations</button>
                     <button className="text-white bg-purple-900 rounded-md h-8 w-24 mt-2 mb-2 hover:font-bold lg:text-2xl lg:w-32">Episodes</button>
                 </div>
