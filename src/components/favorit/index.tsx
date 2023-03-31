0c961b0b8d19d37326dcb1b86fcc803e8c4e08a3
Guilherme Vargas <guisouzavargas@outlook.com>
2023-03-31 15:42:56 -0300

    created component to favorit characters

---
+++ src/components/favorit/index.tsx
@@ -0,0 +1,40 @@
+import { useEffect, useState } from "react"
+
+export default function Favorit(props: any) {
+
+    const name = props.name;
+    let [isFavorit, setIsFavorit] = useState(localStorage.getItem(name));
+
+    useEffect(() => {
+        setIsFavorit(localStorage.getItem(name))
+    }, [localStorage.getItem(name)])
+
+    function Changefavorit() {
+
+        if (localStorage.getItem(name) == null || localStorage.getItem(name) == "false") {
+            localStorage.setItem(name, "true")
+            setIsFavorit("true")
+        }
+
+        else if (localStorage.getItem(name) == "true") {
+            localStorage.setItem(name, "false")
+            setIsFavorit("false")
+        }
+    }
+
+
+    return (
+        <div>
+            {isFavorit == "true" && (
+                <button><img className="w-12 mr-auto ml-auto" src="/img/star.png" onClick={Changefavorit} /></button>
+            )}
+            {isFavorit == "false" && (
+                <button><img className="w-12 mr-auto ml-auto" src="/img/blackstar.png" onClick={Changefavorit} /></button>
+            )}
+            {isFavorit == null && (
+                <button><img className="w-12 mr-auto ml-auto" src="/img/blackstar.png" onClick={Changefavorit} /></button>
+            )}
+        </div>
+    )
+
+}
