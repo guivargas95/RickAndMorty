8d1a4234f8dc5426d2dab09f35d56c2c1f7b6db5
Guilherme Vargas <guisouzavargas@outlook.com>
2023-04-02 15:55:45 -0300

    routes changes

--- src/main.tsx
+++ src/main.tsx
@@ -10,6 +10,7 @@ import './config.css'
 import CharactersPage from './pages/characters';
 import ScrollToTop from './components/scrollToTop';
 import Character from './pages/characters/singleCharacter';
+import FavoritCharacters from './pages/characters/favoritCharacters';
 
 ReactDOM.createRoot(document.getElementById('root') as HTMLElement).render(
   <React.StrictMode>
@@ -19,6 +20,7 @@ ReactDOM.createRoot(document.getElementById('root') as HTMLElement).render(
         <Route path="/" element={<Home />} />
         <Route path="/characters/:page" element={<CharactersPage />} />
         <Route path="/characters/single/:id" element={<Character />} />
+        <Route path="/characters/favorit" element={<FavoritCharacters />} />
       </Routes>
     </BrowserRouter>
   </React.StrictMode>
