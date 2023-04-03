b9c8cb0570f35686b3ebbec54b04318d288f20bb
Guilherme Vargas <guisouzavargas@outlook.com>
2023-04-03 08:05:12 -0300

    fix name

--- src/pages/characters/favoriteCharacters/index.tsx
+++ src/pages/characters/favoriteCharacters/index.tsx
@@ -1,50 +1,50 @@
 import { useEffect, useState } from "react";
 import { Link } from "react-router-dom";
-import Favorit from "../../../components/favorites";
+import Favorites from "../../../components/favorites";
 import { SomeCharacters } from "../../../hooks/someCharacters";
 import { iCharacters } from "../../../types/characters";
 
 export default function FavoritCharacters() {
 
     let local = localStorage;
-    let favoritList: any[] = [];
+    let favoriteList: any[] = [];
     let emptyString = ""
 
     for (let key in local) {
         if (localStorage.getItem(key) == "true") {
-            favoritList.push(key)
+            favoriteList.push(key)
         }
     }
-    if (favoritList.length != 0) {
-        favoritList.forEach(element => {
+    if (favoriteList.length != 0) {
+        favoriteList.forEach(element => {
             emptyString = emptyString + element + ","
         });
     }
 
     let [httpSearchString, setHttpSearchString] = useState(emptyString)
-    let [haveFavorit, setHaveFavorit] = useState(true)
+    let [haveFavorites, setHaveFavorites] = useState(true)
     const [httpLink, setHttpLink] = useState(`https://rickandmortyapi.com/api/character/${httpSearchString}`)
     const { data: characters } = SomeCharacters<iCharacters[]>(httpLink);
 
     useEffect(() => {
-        favoritList = []
+        favoriteList = []
         emptyString = ""
         for (let key in local) {
             if (localStorage.getItem(key) == "true") {
-                favoritList.push(key)
+                favoriteList.push(key)
             }
         }
-        if (favoritList.length != 0) {
-            favoritList.forEach(element => {
+        if (favoriteList.length != 0) {
+            favoriteList.forEach(element => {
                 emptyString = emptyString + element + ","
             });
             setHttpSearchString(emptyString)
             setHttpLink(`https://rickandmortyapi.com/api/character/${httpSearchString}`)
         }
         else {
-            setHaveFavorit(false)
+            setHaveFavorites(false)
         }
-    }, [localStorage, httpLink, httpSearchString, favoritList, local, characters])
+    }, [localStorage, httpLink, httpSearchString, favoriteList, local, characters])
 
 
     return (
@@ -55,7 +55,7 @@ export default function FavoritCharacters() {
             <section className="md:w-auto ml-auto mr-auto">
                 <Link to="/"><img className="w-12 md:w-36 fadeInDown" src="/img/back.png" /></Link>
                 <ul className="flex flex-col justify-center text-center md:grid md:grid-cols-2 lg:grid-cols-3">
-                    {haveFavorit == true && (characters?.map(response => {
+                    {haveFavorites == true && (characters?.map(response => {
                         return (
                             <li className="mr-auto ml-auto bg-orange-500 mt-5 mb-5 md:ml-5 md:mr-5 fadeInDown" key={response.id}>
                                 <Link to={`/characters/single/${response.id}`}><h2 className="flex justify-center items-center w-72 h-24 text-white text-2xl">{response.name}</h2></Link>
@@ -67,13 +67,13 @@ export default function FavoritCharacters() {
                                     {response?.status == "unknown" && (<h3 className="bg-yellow-500 rounded-lg mr-1 ml-1 font-bold">{response.status}</h3>)}
                                 </div>
                                 <div>
-                                    <Favorit name={response.id} />
+                                    <Favorites name={response.id} />
                                 </div>
                             </li>
                         )
                     }))}
                 </ul>
-                {haveFavorit == false && (
+                {haveFavorites == false && (
                     <div className="text-center text-3xl font-bold flex flex-col justify-center">
                         <div>
                             <h2 className="mt-20">Sorry :c You don't have favorits yet</h2>
