aed0a92cdc6dc70d5e7d3e43d6bc3761bfc8f9f0
Guilherme Vargas <guisouzavargas@outlook.com>
2023-04-03 08:45:57 -0300

    test library

---
+++ __tests__/components/favorites/favorites.test.tsx
@@ -0,0 +1,11 @@
+import React from 'react';
+import {render} from '@testing-library/react'
+import Favorites from "../../../src/components/favorites";
+
+describe('MyComponent', () => {
+  test('renders correctly', () => {
+    const { container } = render(<Favorites />);
+
+    expect(container).toMatchSnapshot();
+  });
+});
